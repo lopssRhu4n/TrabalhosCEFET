e67bc441105c3327a7d88b533496717f35c29cc5
Rhuan <rhuanlopesdev@gmail.com>
2023-09-15 16:14:54 -0300

    aula eda - stack

--- EDA/linkedList/main.cpp
+++ EDA/linkedList/main.cpp
@@ -19,55 +19,61 @@ void setupList(List *L) {
   L->last = nullptr;
 }
 
-Node newNode(int val, Node *ptr) {
-  Node node;
-  node.value = val;
-  node.next = ptr == nullptr ? nullptr : ptr;
+Node *newNode(int val, Node *ptr) {
+  Node *node = new Node;
+  node->value = val;
+  node->next = ptr;
 
   return node;
 }
 
-void resolveNodes(Node *savePrev, Node *saveNext, Node *insertNode, int index,
-                  List *list) {
-  Node *current;
-  int listIndex = 0;
-
-  current = list->first;
-
-  while (listIndex <= index) {
-    current = current->next;
-    listIndex++;
-  }
-
-  insertNode->next = current->next;
-  current->next = insertNode;
-}
+// void resolveNodes(Node *savePrev, Node *saveNext, Node *insertNode, int
+// index,
+//                   List *list) {
+//   Node *current;
+//   int listIndex = 0;
+//
+//   current = list->first;
+//
+//   while (listIndex <= index) {
+//     current = current->next;
+//     listIndex++;
+//   }
+//
+//   insertNode->next = current->next;
+//   current->next = insertNode;
+// }
 
 void addToList(int index, int val, List *list) {
-  Node node;
+  Node *node;
+
+  if (index == 0 || list->length == 0) {
+
+    node = newNode(val, list->first);
+    list->first = node;
+
+    if (list->length == 0)
+      list->last = node;
+
+    list->length++;
+    return;
+  }
 
   if (index < 0) {
     node = newNode(val, nullptr);
-    list->last = list->last->next = &node;
+    list->last = list->last->next = node;
     list->length++;
     return;
   }
 
-  if (index == 0) {
-    node = newNode(val, list->first);
-    list->first = &node;
-    list->length++;
-    return;
-  }
-
-  if (index > list->length) {
-    return;
-  }
-
-  Node *prevNode, *nextNode;
-
-  resolveNodes(prevNode, nextNode, &node, index, list);
-  list->length++;
+  // if (index > list->length) {
+  //   return;
+  // }
+  //
+  // Node *prevNode, *nextNode;
+  //
+  // resolveNodes(prevNode, nextNode, &node, index, list);
+  // list->length++;
 }
 
 void printList(List *list) {
@@ -77,7 +83,7 @@ void printList(List *list) {
 
   cout << "[ ";
   for (i = 1; i <= list->length; i++) {
-    cout << cur->value << ", " << cur->next;
+    cout << "(" << cur->value << ", " << cur->next << ")";
     if (cur->next != nullptr) {
       cout << " -> ";
       cur = cur->next;
@@ -90,23 +96,12 @@ void printList(List *list) {
 int main() {
 
   List myList;
-  Node n, n2, n3;
+  Node *n, *n2, *n3;
 
   setupList(&myList);
 
-  n = newNode(5, nullptr);
-
-  n2 = newNode(6, &n);
-
-  myList.first = &n2;
-  myList.length++;
-  myList.length++;
-
-  n3 = newNode(16, &n);
-
-  myList.first->next = &n3;
-  myList.length++;
-
+  addToList(0, 5, &myList);
+  addToList(0, 9, &myList);
   printList(&myList);
 
   return 0;
